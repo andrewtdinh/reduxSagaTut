0e8bb813a5873ef6a613bb9c1ca61e9822b79d26
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-06 23:02:39 -0800

    Modifying main.js one step at a time

--- sagas.js
+++ sagas.js
@@ -1,26 +1,33 @@
 import { takeEvery, takeLatest } from 'redux-saga'
-import { take, put } from 'redux-saga/effects'
-import Api from '...'
+import { put } from 'redux-saga/effects'
+// import Api from 'Api'
+
+export function* incrementAsync(){
+  yield delay(1000)
+  yield put({type: 'INCREMENT'})
+}
+
+
 
 // worker Saga: will be fired on USER_FETCH_REQUESTED actions
-function* fetchUser(action) {
-   try {
-      const user = yield call(Api.fetchUser, action.payload.userId);
-      yield put({type: "USER_FETCH_SUCCEEDED", user: user});
-   } catch (e) {
-      yield put({type: "USER_FETCH_FAILED", message: e.message});
-   }
-}
-
-/*
-  Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
-  Allows concurrent fetches of user.
-*/
-function* mySaga() {
-  yield* takeEvery("USER_FETCH_REQUESTED", fetchUser);
-}
-
-export default mySaga;
+// function* fetchUser(action) {
+//    try {
+//       const user = yield call(Api.fetchUser, action.payload.userId);
+//       yield put({type: "USER_FETCH_SUCCEEDED", user: user});
+//    } catch (e) {
+//       yield put({type: "USER_FETCH_FAILED", message: e.message});
+//    }
+// }
+//
+// /*
+//   Starts fetchUser on each dispatched `USER_FETCH_REQUESTED` action.
+//   Allows concurrent fetches of user.
+// */
+// function* mySaga() {
+//   yield* takeEvery("USER_FETCH_REQUESTED", fetchUser);
+// }
+//
+// export default mySaga;
 
 // export function* helloSaga() {
 //   console.log('Hello Sagas!')
