660b72e83ae8bed4d298158ec1817ae1bf6a9f35
andrewtdinh <andrewdinh74@yahoo.com>
2016-11-06 22:45:54 -0800

    Add fetchUser function

--- sagas.js
+++ sagas.js
@@ -1,6 +1,17 @@
 import { takeEvery, takeLatest } from 'redux-saga'
+import { take, put } from 'redux-saga/effects'
+import Api from '...'
 
-
-export function* helloSaga() {
-  console.log('Hello Sagas!')
+// worker Saga: will be fired on USER_FETCH_REQUESTED actions
+function* fetchUser(action) {
+   try {
+      const user = yield call(Api.fetchUser, action.payload.userId);
+      yield put({type: "USER_FETCH_SUCCEEDED", user: user});
+   } catch (e) {
+      yield put({type: "USER_FETCH_FAILED", message: e.message});
+   }
 }
+
+// export function* helloSaga() {
+//   console.log('Hello Sagas!')
+// }
